ea130d440a240acd1f93ca67a0852a685375d011
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-04-10 14:48:41 +0900

    Chore : Add Storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,25 +6,34 @@ module.exports = {
     es6: true,
   },
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
-  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '/public/*'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
+  parserOptions: {
+    ecmaVersion: 8,
+  },
+  // to enable features such as async/await
+  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '/public/*'],
+  // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@next/next/recommended',
-    'plugin:@typescript-eslint/recommended', // TypeScript rules
-    'plugin:react/recommended', // React rules
-    'plugin:react-hooks/recommended', // React hooks rules
-    'plugin:jsx-a11y/recommended', // Accessibility rules
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'prettier',
     'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
   ],
   overrides: [
     // This configuration will apply only to TypeScript files
     {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
-      settings: { react: { version: 'detect' } },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
       env: {
         browser: true,
         node: true,
@@ -43,20 +52,15 @@ module.exports = {
       rules: {
         'simple-import-sort/imports': 'error',
         'simple-import-sort/exports': 'error',
-
         '@typescript-eslint/no-empty-function': 'off',
-
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-
         '@typescript-eslint/explicit-function-return-type': 'off',
         // We will use TypeScript's types for component props instead
         'react/prop-types': 'off',
         // No need to import React when using Ne xt.js
         'react/react-in-jsx-scope': 'off',
-
         // This rule is not compatible with Next.js's <Link /> components
         'jsx-a11y/anchor-is-valid': 'off',
-
         // Why would you want unused vars?
         '@typescript-eslint/no-unused-vars': 'off',
         'no-template-curly-in-string': 'error',
